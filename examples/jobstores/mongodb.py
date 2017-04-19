b720a12e0d3c66b27de28c9e570e730b36b3e81c
hejiayi <janreyho@gmail.com>
2017-04-19 17:19:22 +0800

    examples/jobstores/mongodb.py success

--- examples/jobstores/mongodb.py
+++ examples/jobstores/mongodb.py
@@ -8,6 +8,7 @@ are still active. Running the example with the --clear switch will remove any ex
 from datetime import datetime, timedelta
 import sys
 import os
+from pytz import utc
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 
@@ -17,12 +18,12 @@ def alarm(time):
 
 
 if __name__ == '__main__':
-    scheduler = BlockingScheduler()
+    scheduler = BlockingScheduler(timezone=utc)
     scheduler.add_jobstore('mongodb', collection='example_jobs')
     if len(sys.argv) > 1 and sys.argv[1] == '--clear':
         scheduler.remove_all_jobs()
 
-    alarm_time = datetime.now() + timedelta(seconds=10)
+    alarm_time = datetime.now() + timedelta(seconds=10) -timedelta(hours=8)
     scheduler.add_job(alarm, 'date', run_date=alarm_time, args=[datetime.now()])
     print('To clear the alarms, run this example with the --clear argument.')
     print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
