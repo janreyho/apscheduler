72f5d77186dcee26620b218504ff4ab66ed12a17
hejiayi <janreyho@gmail.com>
2017-04-19 18:10:11 +0800

    jobstore = MongoDBJobStore(client=client, database='blusql', collection='apscheduljobs')

--- examples/jobstores/mongodb.py
+++ examples/jobstores/mongodb.py
@@ -9,9 +9,10 @@ from datetime import datetime, timedelta
 import sys
 import os
 from pytz import utc
+from pymongo import MongoClient
 
 from apscheduler.schedulers.blocking import BlockingScheduler
-
+from apscheduler.jobstores.mongodb import MongoDBJobStore
 
 def alarm(time):
     print('Alarm! This alarm was scheduled at %s.' % time)
@@ -19,12 +20,15 @@ def alarm(time):
 
 if __name__ == '__main__':
     scheduler = BlockingScheduler(timezone=utc)
-    scheduler.add_jobstore('mongodb', collection='example_jobs')
+    client = MongoClient('mongodb://127.0.0.1:27017/blusql')
+    jobstore = MongoDBJobStore(client=client, database='blusql', collection='apscheduljobs')
+    scheduler.add_jobstore(jobstore)
     if len(sys.argv) > 1 and sys.argv[1] == '--clear':
         scheduler.remove_all_jobs()
 
     alarm_time = datetime.now() + timedelta(seconds=10) -timedelta(hours=8)
     scheduler.add_job(alarm, 'date', run_date=alarm_time, args=[datetime.now()])
+
     print('To clear the alarms, run this example with the --clear argument.')
     print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
 
