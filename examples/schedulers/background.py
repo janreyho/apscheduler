1bb72072b69cc46c97ee24dff98794076f2301ed
hejiayi <janreyho@gmail.com>
2017-05-02 12:25:26 +0800

    'cron'  'date'定时均为UTC时间

--- examples/schedulers/background.py
+++ examples/schedulers/background.py
@@ -14,10 +14,19 @@ from apscheduler.schedulers.background import BackgroundScheduler
 def tick():
     print('Tick! The time is: %s' % datetime.now())
 
+def tick1():
+    print('Tick2! The time is: %s' % datetime.now())
+
+def tick2():
+    print('Tick2! The time is: %s' % datetime.now())
 
 if __name__ == '__main__':
     scheduler = BackgroundScheduler(timezone=utc)
+
     scheduler.add_job(tick, 'interval', seconds=3)
+    scheduler.add_job(tick1, 'cron', id='my_test_job3', day_of_week='0-6', hour='4', minute='23')   #UTC时间
+    scheduler.add_job(tick2, 'date', run_date=datetime(2017, 05, 02, 4, 23, 0))                     #UTC时间
+    # trigger = CronTrigger(year='2009', month='2', hour='8-10', start_date='2009-02-03 11:00:00', timezone=timezone)
     scheduler.start()
     print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
 
